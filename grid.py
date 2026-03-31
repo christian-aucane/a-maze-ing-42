db85f8c0696e47faa279780fc566cb6d6f18784d
Christian Aucane <christianaucane1@gmail.com>
2026-04-01 01:31:44 +0200

    add utils methods

--- grid.py
+++ grid.py
@@ -41,54 +41,56 @@ class Box:
         dx, dy = direction.value
         return (self.x + dx, self.y + dy)
 
+    def get_open_directions(self, box: "Box", previous_direction: Direction):
+        return tuple(dir for dir, wall in self.walls.items() if not wall)
 
-class GridError(Exception): ...
+
+class GridError(Exception):
+    pass
 
 
 class Grid:
-    def _generate_grid(self):
+    def _generate_grid(self) -> list[list[Box]]:
         grid = []
         for y in range(self.height):
             grid.append([Box(x, y) for x in range(self.width)])
         return grid
 
-    def is_bounded(self, x: int, y: int) -> bool:
-        return 0 <= x < self.width and 0 <= y < height
-
-    def get_box(self, x: int, y: int) -> box:
-        if self.is_bounded(x, y):
-            return self.grid[y][x]
-        raise GridError(f"Grid error at (x={x}, y={y}): Box is out of bound")
-
-    def get_neighbour(self, box: Box, direction: Direction):
+    def _get_neighbour(self, box: Box, direction: Direction) -> Box | None:
         x, y = box.get_neighbour_pos(direction)
         try:
             return self.get_box(x, y)
         except GridError:
             return None
 
-    def break_wall(self, box: Box, direction: Direction): ...
+    def _get_oposite_direction(self, direction: Direction) -> Direction:
+        return {
+            Direction.NORTH: Direction.SOUTH,
+            Direction.SOUTH: Direction.NORTH,
+            Direction.EAST: Direction.WEST,
+            Direction.WEST: Direction.EAST,
+        }[direction]
 
-    def __init__(self, width: int, height: int):
+    def _is_bounded(self, x: int, y: int) -> bool:
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
         self.grid = self._generate_grid()
 
-    ...
+    def get_box(self, x: int, y: int) -> Box:
+        if self._is_bounded(x, y):
+            return self.grid[y][x]
+        raise GridError(f"Grid error at (x={x}, y={y}): Box is out of bound")
 
-    ...
+    def get_boxes(self) -> list[Box]:
+        return [box for row in self.grid for box in row]
 
-
-class Grid:
-    ...
-
-    ...
-
-
-class Grid:
-    ...
-
-    ...
-
-
-class Grid: ...
+    def break_wall(self, box: Box, direction: Direction) -> bool:
+        neighbour = self._get_neighbour(box, direction)
+        if neighbour is not None:
+            box.break_wall(direction)
+            neighbour.break_wall(self._get_oposite_direction())
+            return True
+        return False
