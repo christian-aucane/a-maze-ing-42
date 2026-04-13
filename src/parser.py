28c4978d4a0085264d152a3fdd038511cebb674b
samet <sametoussama13@gmail.com>
2026-04-13 23:28:25 +0200

    parsing

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,16 @@ from .common import Config
 
 
 def parse_config_file(config_file_path: str) -> Config | None:
+    list_parse: 
+    config: dict = {}
+    with open(config_file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            key, value = line.split("=")
+            config[key] = value
+
     config = Config.read_file(config_file_path)
     if config is None:
         print(f"Config file '{config_file_path}' doesn't exist...")
