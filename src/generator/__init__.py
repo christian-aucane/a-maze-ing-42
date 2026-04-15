d7c6a70ac64a64842bf1ab3a4fce8474b1ce9f12
Christian Aucane <caucane@c1r9s1.marseille.42.school>
2026-04-15 18:01:07 +0200

    fix

---
+++ src/generator/__init__.py
@@ -0,0 +1,3 @@
+from .maze_generator import generate_maze, MazeGenerator
+
+__all__ = ["generate_maze", "MazeGenerator"]
--- src/generator/maze_generator.py
+++ src/generator/maze_generator.py
@@ -1,47 +1,72 @@
-from typing import Optional
+from abc import ABC, abstractmethod
+from typing import Any, Optional
 
 from src.grid import MazeGrid
 from src.config import Config
-from .algorithms import GENERATION_ALGORITHMS_CLASSES
+
+
+class GenerationAlgorithm(ABC):
+    def __init__(self, is_perfect: bool, *args: Any, **kwargs: Any):
+        self.is_perfect = is_perfect
+
+    @abstractmethod
+    def run(
+        self, grid: MazeGrid, start: tuple[int, int], end: tuple[int, int]
+    ) -> bool: ...
 
 
 class MazeGenerator:
-    def __init__(self, algo_name: str):
-        try:
-            self.algo_class = GENERATION_ALGORITHMS_CLASSES[algo_name]
-        except KeyError:
-            raise ValueError(
-                "Invalid algorithm name ... Valid names: "
-                f"{GENERATION_ALGORITHMS_CLASSES.keys()}"
-            )
+    """
+    Algo sample
+    'dfs': {
+        'class': DfsAlgorithm,
+        'args': [args...],     # Optionnal
+        'kwargs': {kwargs...}  # Optionnal
+    """
+
+    ALGORITHMS = {"...": {"class": GenerationAlgorithm}}
+    # TODO: separer l'algo et faire de la composition ?
+
+    def __init__(self, algo: GenerationAlgorithm):
+        self.algo = algo
 
     def generate_maze(
         self,
         size: tuple[int, int],
         start: tuple[int, int],
         end: tuple[int, int],
-        is_perfect: bool,
     ) -> Optional[MazeGrid]:
         grid = MazeGrid(*size)
-        algo = self.algo_class(
-            grid=grid, start=start, end=end, is_perfect=is_perfect
-        )
-        if algo.run():
+        if self.algo.run(grid=grid, start=start, end=end):
             return grid
         return None
 
+    @classmethod
+    def get_generator(
+        cls, algo_name: str, is_perfect: bool
+    ) -> Optional["MazeGenerator"]:
+        try:
+            algo_dict = cls.ALGORITHMS[algo_name]
+            algo = algo_dict["class"](
+                is_perfect=is_perfect,
+                *algo_dict.get("args", []),
+                **algo_dict.get("kwargs", {}),
+            )
+            return cls(algo=algo)
+        except KeyError:
+            return None
+
 
 def generate_maze(config: Config) -> MazeGrid | None:
-    generator = MazeGenerator(algo_name=config.gen_algo)
+    generator = MazeGenerator.get_generator(
+        algo_name=config.gen_algo, is_perfect=config.perfect
+    )
     if generator is None:
         print("Error durring generator instanciation...")
         return None
 
     grid = generator.generate_maze(
-        size=(config.width, config.height),
-        start=config.start,
-        end=config.end,
-        is_perfect=config.perfect,
+        size=(config.width, config.height), start=config.start, end=config.end
     )
     if grid is None:
         print("Error durring maze generation...")
