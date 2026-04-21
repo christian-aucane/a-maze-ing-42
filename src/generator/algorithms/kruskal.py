bfeb8526e0d3cf3bf4cee1e12795a164b79bccce
Christian Aucane <christianaucane1@gmail.com>
2026-04-21 20:58:02 +0200

    implement krustal

--- src/generator/algorithms/kruskal.py
+++ src/generator/algorithms/kruskal.py
@@ -1,5 +1,44 @@
+import random
+
 from .abstract import GenerationAlgorithm
+from src.common import Direction
+from src.grid import OutOfBoundError, MazeBox, MazeGrid
 
 
 class Kruskal(GenerationAlgorithm):
-    def run(self) -> bool: ...
+    def __init__(self, grid: MazeGrid, is_perfect: bool):
+        super().__init__(grid=grid, is_perfect=is_perfect)
+        self.parents = {box: box for box in self.grid.get_boxes()}
+
+    def find(self, cell: MazeBox):
+        current = cell
+        while current != self.parents[current]:
+            current = self.parents[current]
+        return current
+
+    def union(self, cell_a: MazeBox, cell_b: MazeBox):
+        root_a = self.find(cell_a)
+        root_b = self.find(cell_b)
+        self.parents[root_b] = root_a
+
+    def are_connected(self, cell_a, cell_b):
+        return self.find(cell_a) == self.find(cell_b)
+
+    def run(self) -> bool:
+        walls = []
+        for cell in self.grid.get_boxes():
+            for dir in [Direction.SOUTH, Direction.EAST]:
+                try:
+                    neighbour = self.grid.get_neighbour(box=cell, direction=dir)
+                    walls.append((cell, neighbour, dir))
+                except OutOfBoundError:
+                    continue
+
+        random.shuffle(walls)
+        for cell_a, cell_b, dir in walls:
+            if self.are_connected(cell_a, cell_b):
+                continue
+            self.union(cell_a, cell_b)
+            self.grid.break_wall(cell_a, dir)
+
+        return True
