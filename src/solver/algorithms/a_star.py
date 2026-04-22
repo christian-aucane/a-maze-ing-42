84e5c4a77648d0ccd33b97df7c32750698a8f8f7
Christian Aucane <christianaucane1@gmail.com>
2026-04-23 00:37:33 +0200

    implement a* naive version (not optimised)

--- src/solver/algorithms/a_star.py
+++ src/solver/algorithms/a_star.py
@@ -7,24 +7,24 @@ class AStar(SolvingAlgorithm):
     def __init__(self, grid: MazeGrid):
         super().__init__(grid)
         start = self.grid.entry
-        self.open_set: set[MazeBox] = {start}
-        self.closed_set: set[MazeBox] = set()
-        self.g_score: dict[MazeBox, int] = {start: 0}
-        self.parents: dict[MazeBox, MazeBox | None] = {start: None}
+        self.open_set = {start}
+        self.closed_set = set()
+        self.g_score = {start: 0}
+        self.parents = {start: None}
 
-    def get_h(self, cell: MazeBox) -> int:
+    def get_h(self, cell: MazeBox):
         return abs(cell.x - self.grid.exit.x) + abs(cell.y - self.grid.exit.y)
 
-    def get_g(self, cell: MazeBox) -> int:
+    def get_g(self, cell: MazeBox):
         return self.g_score[cell]
 
-    def get_f(self, cell: MazeBox) -> int:
+    def get_f(self, cell: MazeBox):
         return self.get_h(cell) + self.get_g(cell)
 
-    def get_best_cell(self) -> MazeBox:
+    def get_best_cell(self):
         return min(self.open_set, key=self.get_f)
 
-    def expand(self, current: MazeBox) -> None:
+    def expand(self, current: MazeBox):
         self.open_set.remove(current)
         self.closed_set.add(current)
         for cell in self.grid.get_open_neighbours(current):
@@ -44,6 +44,8 @@ class AStar(SolvingAlgorithm):
             return Direction((cell_b.x - cell_a.x, cell_b.y - cell_a.y))
         except ValueError:
             return None
+        # TODO: Retrouver la direction !!
+        return Direction((cell_b.x - cell_a.x, cell_b.y - cell_a.y))
 
     def run(self) -> list[Direction]:
         while self.open_set:
@@ -52,12 +54,12 @@ class AStar(SolvingAlgorithm):
                 break
             self.expand(current)
         if self.grid.exit not in self.parents:
-            return []
-        cells: list[MazeBox] = []
+            return []  # TODO:  RETOURNER None en cas de sortie pas trouvee ??
+        cells = []
         current = self.grid.exit
         while current is not None:
             cells.insert(0, current)
-            current = self.parents[current]  # type: ignore
+            current = self.parents[current]
         solution = []
         for i in range(len(cells) - 1):
             solution.append(self.get_direction(cells[i], cells[i + 1]))
