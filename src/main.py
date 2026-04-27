bcf910f5c972f35a8c98da1037cf8d7218a4ed31
Oussama SAMET <osamet@c1r8s2.marseille.42.school>
2026-04-27 15:06:02 +0200

    change input str for nbr in main

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def run(config_file_path: str) -> int:
     # print(f"SOLUTION:\n{solution}\n")
 
     while 1:
-        nbr = int(input("1- Regenerate Maze: \n"
+        nbr = int(input("\n1- Regenerate Maze: \n"
                         "2- show and hide solution from entry to exit: \n"
                         "3- Change walls color: \n"
                         "4- change pattern color: \n"))
@@ -56,15 +56,23 @@ def run(config_file_path: str) -> int:
                 maze.hide_solution = True
 
         elif nbr and nbr == 3:
-            color = input("input color: ")
-            if maze.change_colors_walls(color, nbr):
-                print(f"MAZE:\n{maze.get_debug()}\n")
+            while 1:
+                color = input("input color or exit: ")
+                if color == "exit":
+                    break
+                if maze.change_colors_walls(color, nbr):
+                    print(f"MAZE:\n{maze.get_debug()}\n")
+                    break
 
         elif nbr and nbr == 4:
-            color = input("input color: ")
-            if maze.change_colors_walls(color, nbr):
-                print(f"MAZE:\n{maze.get_debug()}\n")
-
+            while 1:
+                color = input("input color or exit: ")
+                if color == "exit":
+                    break
+                if maze.change_colors_walls(color, nbr):
+                    print(f"MAZE:\n{maze.get_debug()}\n")
+                    break
+    
             # TODO: write output file, run UI (cli or gui)
             # (move all in ui ? can restart ...etc )
     return 0
